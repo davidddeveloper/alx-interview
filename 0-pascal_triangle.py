202e543a127128b34b288a7af38859e70750139f
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-11 20:55:03 +0000

    Create 0-pascal_triangle.py

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,26 @@
+def pascal_triangle(n):
+    """ generate a pascal triangle base on the n
+    
+        - n: the height or number of row in the computed pascal triangle
+
+    """
+
+    pascal_tri = []
+    if n <= 0:
+        return pascal_tri
+
+    prev_row = []
+    for i in range(n):
+        j = 0
+        row = []
+        while j <= i:
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                column = prev_row[j] + prev_row[j - 1]
+                row.append(column)
+            j += 1
+        pascal_tri.append(row)
+        prev_row = row
+    
+    return pascal_tri
