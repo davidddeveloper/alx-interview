7b6a39eae980332457273d598543557a7366e720
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-17 20:22:37 +0000

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+"""
+  0-lockboxes: represent a lockboxes solution with loops
 
+"""
+
+def canUnlockAll(boxes):
+    """ True if all boexes can be opened
+
+        False otherwise
+    """
+    viewedboxes = {0}  # keys of boxes
+    for idx, val in enumerate(boxes):
+        if idx not in viewedboxes and idx > 0:
+            for key in val:
+                viewedboxes.add(key)
+
+    if len(viewedboxes) == len(boxes):
+        return True
+    return False
