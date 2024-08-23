0183b948efd545e8895685fd624c06a1a2fe7b72
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-23 10:35:38 +0000

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+  0-minoperations.py: Minimum Operations
+  read the readme for explanation here
+  https://github.com/davidddeveloper/alx-interview/blob/main/0x02-minimum_operations/README.md
+
+"""
+
+
+def minOperations(n):
+    no_of_operation = 0
+    if n == 1:
+        # initially create the file
+        with open('xyz', 'w') as f:
+            f.write('H')  # paste
+        return no_of_operation
+
+    # dynamic programming - tabulation
+    for i in range(2, n + 1):
+        if n % i == 0:
+            print('factor', i)
+            no_of_operation += minOperations(n // i)
+            no_of_operation += i
+
+            # perform the read and write operation
+            copied = 'character'
+            for i in range(i):
+                if i == 0:
+                    with open('xyz', 'r') as f:
+                        copied = f.read()
+
+                else:
+                    with open('xyz', 'a') as f:
+                        f.write(copied)
+
+            break
+
+    return no_of_operation
