39f78db7cbf388fe7a22c1092c3aeb34f941a1ee
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-23 21:03:26 +0000

    Create iterative_approach.py

---
+++ 0x02-minimum_operations/iterative_approach.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    0-minoperations: iterative approach to find the minimum number operations
+    to result in nHs
+    read the readme for explanation at github:
+    davidddeveloper/alx-interview/blob/main/0x02-minimum_operations/README.md
+
+"""
+
+
+def minOperations(n):
+    operation_count = 0
+    if n == 1:
+        return operation_count
+
+    counter = 2
+    while (counter <= n):
+        while n % counter == 0:  # counter is a factor of n
+            operation_count += counter
+
+            n = n // counter
+        counter += 1
+
+    return operation_count
+  
