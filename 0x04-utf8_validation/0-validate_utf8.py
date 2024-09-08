cebbc263f8d0eb5e65df0132c73721ae81721fde
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-08 01:30:30 +0000

    pycodestyle fixed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
     this code checks if a data is a valid utf-8 encoded
+    len(bin(number).split('0b')[1]) > 7
 """
 
 
@@ -9,6 +10,6 @@ def validUTF8(data):
         return False
 
     for number in data:
-        if len(bin(number).split('0b')[1]) > 7:
+        if number < 32 or number > 128:
             return False
     return True
