db453e4bdabcd4e145d9ad3693aea36529242ffb
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-20 21:03:21 +0000

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,28 @@
 #!/usr/bin/node
+
 const argv = process.argv.slice(2);
 const filmId = argv[0];
-const endpoint = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+const request = require('request');
 
-fetch(endpoint)
-  .then((response) => {
-    return response.json();
-  })
-  .then(data => {
-    if (data.characters !== undefined) {
-      async function getCharacter (characters) {
-        for (const character of characters) {
-          await fetch(character)
-            .then(response => {
-              return response.json();
-            })
-            .then(data => {
-              console.log(data.name);
-            }).catch(() => {});
-        }
+request(`https://swapi-api.alx-tools.com/api/films/${filmId}`, { json: true }, (err, res, body) => {
+  if (!err && res.statusCode === 200) {
+    const characters = body.characters;
+    const fetchCharacter = (characterUrl) => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, { json: true }, (err, res, body) => {
+          if (!err && res.statusCode === 200) {
+            resolve(body);
+          }
+        });
+      });
+    };
+
+    async function fetchData () {
+      for (const character of characters) {
+        const res = await fetchCharacter(character);
+        console.log(res.name);
       }
-      getCharacter(data.characters);
     }
-  })
-  .catch(() => {});
+    fetchData();
+  }
+});
