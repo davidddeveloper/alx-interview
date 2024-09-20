8f636e7872f7159891c24d96d0c715ded7856f3e
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-20 21:02:32 +0000

    Update with_fetch.js

--- 0x06-starwars_api/with_fetch.js
+++ 0x06-starwars_api/with_fetch.js
@@ -1,1 +1,26 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+const filmId = argv[0];
+const endpoint = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
 
+fetch(endpoint)
+  .then((response) => {
+    return response.json();
+  })
+  .then(data => {
+    if (data.characters !== undefined) {
+      async function getCharacter (characters) {
+        for (const character of characters) {
+          await fetch(character)
+            .then(response => {
+              return response.json();
+            })
+            .then(data => {
+              console.log(data.name);
+            }).catch(() => {});
+        }
+      }
+      getCharacter(data.characters);
+    }
+  })
+  .catch(() => {});
