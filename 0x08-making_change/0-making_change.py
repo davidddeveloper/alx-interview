dd4a2faf76eab1c0cbdb376afd12b41bb0748b77
davidddeveloper <programmingdonebydavid@gmail.com>
2024-10-04 01:43:27 +0000

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+    0-making_change.py: minimum set of coins needed to make a change
+"""
+
+def makeChange(coins, total):
+    """
+        return the minimum set of coins needed
+        to make a change to a total
+    """
+    dp = coins
+    dp.sort()
+    count = 0
+
+    i = len(dp)
+    while i >= 0:
+        if dp[i - 1] <= total:
+            total = total - dp[i - 1]
+            count += 1
+        elif total == 0:
+            break
+        else:
+            i = i - 1
+
+    return count
+
+print(makeChange([1, 2, 25], 37))
