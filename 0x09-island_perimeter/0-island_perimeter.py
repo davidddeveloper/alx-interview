5c139e1676b62c36dca71461c7f0b31fc67c9547
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-10-10 12:54:24 +0000

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -33,7 +33,7 @@ def island_perimeter(grid):
 
     if vertical_start == (0, 0) and horizontal_start == (0, 0) \
             and vertical_end == (0, 0) and horizontal_end == (0, 0):
-        return 0
+        return 0  # no island
 
     # we are adding 1 for inclusivity
     vertical_length = ((vertical_end[0] - vertical_start[0]) + 1)
